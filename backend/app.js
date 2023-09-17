f2f2901048809d5306e88cbba8a4f9e23de2d401
jamalshahzeepalm <jamalshah@zeepalm.com>
2023-09-17 09:38:29 +0500

    8th commit

--- backend/app.js
+++ backend/app.js
@@ -7,47 +7,21 @@ import dotenv from "dotenv";
 
  const app= express();
   dotenv.config();
-// mongoose.connect().then((result) => {
+mongoose.connect(process.env.DATABASE_URL).then((result) => {
     
-// app.get('/',  function(req,res){
-//     var response={message:"'app is live now...'"}
-//     res.send(response);
-// });
+app.get('/',  function(req,res){
+    var response={message:"'app is live now...'"}
+    res.send(response);
+});
  
-// }).catch((err) => {
-//    console.log(err); 
-// });
-const PORT = process.env.PORT || 3000
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.DATABASE_URL);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-    app.get('/',  function(req,res){
-          var response={message:"'app is live now...'"}
-          res.send(response);
-      });
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
-}
+}).catch((err) => {
+   console.log(err); 
+});
 
-//Routes go here
-app.all('*', (req,res) => {
-    res.json({"every thing":"is awesome"})
-})
+// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
 
 // parse application/json
 app.use(bodyParser.json())
 app.use('/note',noteRoute);
-//Connect to the database before listening
-connectDB().then(() => {
-
-    app.listen(PORT, () => {
-        console.log("listening for requests");
-    })
-})
-// parse application/x-www-form-urlencoded
-
- 
+app.listen(3000,()=>console.log('sever is running'))
