fc1df95f669e6f5f402ab7dc63a88cfa34a8999e
jamalshahzeepalm <jamalshah@zeepalm.com>
2023-09-17 09:49:57 +0500

    10th commit

--- backend/app.js
+++ backend/app.js
@@ -2,29 +2,31 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
- import noteRoute from "./routers/note.js";
- import NM from "./model/note_model.js";
+import noteRoute from "./routers/note.js";
 
- const app= express();
-  dotenv.config();
-  // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }))
+const app = express();
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // parse application/json
-app.use(bodyParser.json())
-app.use('/note',noteRoute);
+app.use(bodyParser.json());
+app.use("/note", noteRoute);
+dotenv.config();
+app.get('/',(req,res)=>{
+  res.send("Api is Working...")
+})
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.DATABASE_URL);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
 
-mongoose.connect(process.env.DATABASE_URL).then((result) => {
-    
-  app.listen(3000,()=>console.log('sever is running'))
- 
-}).catch((err) => {
-   console.log(err); 
-});
-
-app.get('/',  function(req,res){
-  var response={message:"'app is live now...'"}
-  res.send(response);
-});
-
-
+connectDB().then(() => {
+  app.listen(3000, () => {
+      console.log("listening for requests");
+  })
+})
