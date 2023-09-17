3f13c1f0a7a8f127126ddca47b062b27350f9d3e
jamalshahzeepalm <jamalshah@zeepalm.com>
2023-09-17 08:48:31 +0500

    6th commit

--- backend/app.js
+++ backend/app.js
@@ -9,21 +9,11 @@ import dotenv from "dotenv";
   dotenv.config();
 mongoose.connect(process.env.DATABASE_URL).then((result) => {
     
-app.get('/list',  function(req,res){
+app.get('/',  function(req,res){
     var response={message:"'app is live now...'"}
     res.send(response);
 });
-// app.get('/list/ ',async function(req,res){
-//     var note= new NM({
-//         id:"'0002",
-//         userId:"jamalkhanii691",
-//         title:"this is my title",
-//         description:"this is my des"
-//     });
-//    await note.save();
-//     const response={massage:"new data created"};
-//     res.json(response);
-// })
+ 
 }).catch((err) => {
    console.log(err); 
 });
